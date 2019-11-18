588eed6e8fde7b1d26f48985a93d7fee4613f571
Descalon <info@nicoglas.com>
2019-11-18 21:22:51 +0100

    added files for duplication

---
+++ unitMetricsTest/src/unitMetricsTest/Duplication1.java
@@ -0,0 +1,15 @@
+package unitMetricsTest;
+
+public class Duplication1 {
+	public int fib(int n) throws IllegalArgumentException {
+		if(n < 0) throw new IllegalArgumentException("Fib needs zero or a positive integer");
+		if(n == 0) return 0; if(n == 1) return 1;
+		else 
+			return fib(n-1) + fib(n-2);
+	}
+	
+	public int someRandomFunction() {
+		return 5;
+	}
+	
+}
